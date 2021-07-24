60459c2f04e98b8685a81e7dbee1eff14cf23cb0
Luiz Manke <luiz.manke@neoway.com.br>
2021-07-24 10:55:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,7 @@
 from setuptools import setup, find_packages
-from relation_detection._version import __version__
 
 
 setup(
     name="relation_detection",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description="long_description",
-    long_description_content_type="text/markdown",
-    url="https://github.com/luizmanke/relation-detection",
-    project_urls={
-        "Bug Tracker": "https://github.com/luizmanke/relation-detection/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "relation_detection"},
-    packages=find_packages(),
-    python_requires=">=3.6",
+    packages=find_packages(exclude=("tests"))
 )
