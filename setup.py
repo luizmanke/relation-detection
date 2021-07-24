ee9a13b70400998ad2228c5f0edbad6df2aa25c8
Luiz Manke <luiz.manke@neoway.com.br>
2021-07-24 11:20:45 -0300

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 
 
+with open("requirements.txt") as req_file:
+    dependencies = req_file.read().splitlines()
+
 setup(
     name="relation_detection",
-    packages=find_packages(exclude=("tests"))
+    packages=find_packages(exclude=("tests")),
+    install_requires=dependencies
 )
