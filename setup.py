d3d1d5a320ef7160852a717ca25d32b6320889e4
Luiz Manke <luiz.manke@neoway.com.br>
2021-07-24 11:29:20 -0300

    Ignore dev dependencies

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
 
+# get dependencies
 with open("requirements.txt") as req_file:
     dependencies = req_file.read().splitlines()
+
+# ignore dev dependencies
+test_index = dependencies.index("# testing")
+dependencies = dependencies[:test_index]
 
 setup(
     name="relation_detection",
