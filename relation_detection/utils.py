2c9bcb797b08be0784adc2b49380e35252074d04
Luiz Manke <luiz.manke@neoway.com.br>
2021-08-09 19:42:19 -0300

    Refactor LIME explainer

--- relation_detection/utils.py
+++ relation_detection/utils.py
@@ -1,4 +1,3 @@
-import copy
 import nltk
 import numpy as np
 from lime.lime_text import LimeTextExplainer
@@ -7,63 +6,46 @@ from typing import Any, List
 
 def explain_with_lime(model: Any, sample: dict) -> None:
 
-    sample = copy.deepcopy(sample)
+    sample = dict(sample)
 
+    # get original tokens
     sample["tokens"][sample["index_1"]] = "[E1]"
     sample["tokens"][sample["index_2"]] = "[E2]"
     original_sentence = " ".join(sample["tokens"])
     original_tokens = _tokenize_to_words(original_sentence)
-    index_1, index_2 = None, None
+
+    # get entities location
     for i, token in enumerate(original_tokens):
         if token == "[E1]":
             index_1 = i
         if token == "[E2]":
             index_2 = i
-    assert index_1 is not None and index_2 is not None
 
+    # create sentence without entities
     sample["tokens"][sample["index_1"]] = ""
     sample["tokens"][sample["index_2"]] = ""
     sentence = " ".join(sample["tokens"])
 
-    def _predict(sentences: List[str], index_1=index_1, index_2=index_2) -> np.ndarray:
+    # define function
+    def _predict(
+            sentences: List[str],
+            index_1: int = index_1,
+            index_2: int = index_2
+    ) -> np.ndarray:
 
-        sentences_reordered = []
+        # Create samples
+        samples = []
         for sentence in sentences:
             tokens = _tokenize_to_words(sentence)
             for index, element in sorted(zip([index_1, index_2], ["[E1]", "[E2]"])):
                 tokens.insert(index, element)
-            sentences_reordered.append(" ".join(tokens))
-        
-        tokens_without_pad = [
-            model.model_.tokenizer_.tokenize(sentence) for sentence in sentences_reordered]
-
-        max_length = len(tokens_without_pad[0])
-        tokens_with_pad = [
-            tokens + ["[PAD]"] * (max_length - len(tokens)) for tokens in tokens_without_pad]
-
-        ids = []
-        for tokens in tokens_with_pad:
-            tokens = tokens[:model.model_.max_sequence_length_ - 2]
-            ids.append(model.model_.tokenizer_.build_inputs_with_special_tokens(
-                model.model_.tokenizer_.convert_tokens_to_ids(tokens)
-            ))
-
-        samples_tokenized = []
-        for id in ids:
-            a, b = None, None
-            for i, token in enumerate(id):
-                if token == 29794:
-                    a = i + 1
-                if token == 29794:
-                    b = i + 1
-            assert a is not None and b is not None
-            samples_tokenized.append({
-                "tokens": id,
-                "index_1": a,
-                "index_2": b
+            samples.append({
+                "tokens": tokens,
+                "index_1": index_1,
+                "index_2": index_2
             })
 
-        return model.model_._predict_proba(samples_tokenized)
+        return model.predict(samples, return_proba=True, for_lime=True)
 
     explainer = LimeTextExplainer(
         class_names=["not related", "related"],
