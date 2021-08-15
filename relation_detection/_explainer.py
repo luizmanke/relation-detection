c3c4b5cfc961e64c0502ea95209c1dd1b6a7895b
Luiz Manke <luiz.manke@neoway.com.br>
2021-08-15 16:25:24 -0300

    Add original sentence to explanation

--- relation_detection/_explainer.py
+++ relation_detection/_explainer.py
@@ -1,7 +1,7 @@
 import nltk
 import numpy as np
 from lime.submodular_pick import SubmodularPick
-from lime.lime_text import LimeTextExplainer
+from lime.lime_text import IndexedString, LimeTextExplainer, TextDomainMapper
 from typing import Any, Callable, List, Tuple
 from . import utils
 
@@ -13,6 +13,7 @@ class Explainer:
 
     def explain_sample(self, model: Any, sample: dict) -> None:
         original_tokens = self._get_original_tokens(sample)
+        original_sentence = " ".join(original_tokens)
         index_1, index_2 = self._find_entities_index(original_tokens)
 
         # define nested function
@@ -20,8 +21,14 @@ class Explainer:
             samples = self._recreate_samples(sentences, index_1, index_2)
             return model.predict(samples, return_proba=True, for_lime=True)
 
-        sentence = self._create_sentence_without_entities(sample)
-        self._explain_sample(sentence, _nested_predict)
+        sentence_without_entities = self._create_sentence_without_entities(sample)
+        self._explain_sample(
+            sentence_without_entities,
+            _nested_predict,
+            original_sentence,
+            index_1,
+            index_2
+        )
 
     def explain_model(self, model: Any, samples: List[dict]) -> None:
         globals()["i"] = -1
@@ -77,12 +84,37 @@ class Explainer:
             })
         return samples
 
-    def _explain_sample(self, sentence: str, predict_function: Callable) -> None:
+    def _explain_sample(
+            self,
+            sentence: str,
+            predict_function: Callable,
+            original_sentence: str,
+            index_1: int,
+            index_2: int
+    ) -> None:
+
+        # Explain
         explainer = self._create_explainer()
         lime_values = explainer.explain_instance(
             text_instance=sentence,
             classifier_fn=predict_function
         )
+
+        # Replace sentence
+        lime_values.domain_mapper = TextDomainMapper(IndexedString(
+            original_sentence,
+            split_expression=self._tokenize_to_words,
+            mask_string="[PAD]",
+            bow=False
+        ))
+        new_local_exp = {1: [[x[0], x[1]] for x in lime_values.local_exp[1]]}
+        for item in new_local_exp[1]:
+            for index in sorted([index_1, index_2]):
+                if item[0] >= index:
+                    item[0] += 1
+        lime_values.local_exp = new_local_exp
+
+        # Plot
         lime_values.show_in_notebook(
             text=True,
             labels=(lime_values.available_labels()[0],)
