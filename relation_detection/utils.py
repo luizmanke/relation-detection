b2cc0723a15ff05e033ec5446f7f3d64f31e53b4
Luiz Manke <luiz.manke@neoway.com.br>
2021-08-15 14:15:51 -0300

    Create print_sentence function

--- relation_detection/utils.py
+++ relation_detection/utils.py
@@ -1,8 +1,9 @@
 import nltk
 import spacy
+from termcolor import colored
 
 
-def download_nltk_model():
+def download_nltk_model() -> None:
     try:
         nltk.data.find("corpora/stopwords")
         nltk.data.find("tokenizers/punkt")
@@ -13,8 +14,16 @@ def download_nltk_model():
         print("")
 
 
-def download_spacy_model():
+def download_spacy_model() -> None:
     if not spacy.util.is_package("pt_core_news_lg"):
         print("\nDownloading spacy model...")
         spacy.cli.download("pt_core_news_lg", False, False, "--quiet")  # type: ignore
         print("")
+
+
+def print_sentence(sample: dict) -> None:
+    tokens = [token for token in sample["tokens"]]
+    tokens[sample["index_1"]] = colored(tokens[sample["index_1"]], "blue", attrs=["bold"])
+    tokens[sample["index_2"]] = colored(tokens[sample["index_2"]], "red", attrs=["bold"])
+    sentence = " ".join(tokens)
+    print(sentence)
