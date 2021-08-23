ede893aa11d88b1e1f80efa5385a331a92fba36a
Luiz Manke <luiz.manke@neoway.com.br>
2021-08-23 13:45:45 -0300

    Add entity names to SHAP

--- relation_detection/explainers/shap.py
+++ relation_detection/explainers/shap.py
@@ -10,37 +10,25 @@ class SHAP(BaseExplainer):
         pass
 
     def explain_samples(self, model: Any, samples: List[dict]) -> None:
-        shap_values = self._loop_samples(model, samples)
-        shap.plots.text(shap_values[:, :, 1])
+        if len(samples) == 1:
+            shap_values = self._explain_sample(model, samples[0])
+        else:
+            shap_values = self._loop_samples(model, samples)
+        for i in range(len(shap_values)):
+            shap.plots.text(shap_values[i, :, 1])
 
     def explain_model(self, model: Any, samples: List[dict]) -> None:
         shap_values = self._loop_samples(model, samples)
-        shap.plots.bar(shap_values[:, :, 1])
+        shap.plots.bar(shap_values[:, :, 1], max_display=20)
 
     def _loop_samples(self, model: Any, samples: List[dict]) -> shap.Explanation:
-        values: List[str] = []
-        base_values: List[str] = []
-        data: List[str] = []
-        feature_names: List[str] = []
-        hierarchical_values: List[str] = []
-        clustering: List[str] = []
-
-        for sample in samples:
-            raw_shap_value = self._explain_sample(model, sample)
-            values.append(raw_shap_value.values[0, :, :])
-            base_values.append(raw_shap_value.base_values[0, :])
-            data.append(raw_shap_value.data[0, :])
-            feature_names.append(raw_shap_value.feature_names[0])
-            hierarchical_values.append(raw_shap_value.hierarchical_values[0, :, :])
-            clustering.append(raw_shap_value.clustering[0, :, :])
-
+        shap_values = [self._explain_sample(model, sample) for sample in samples]
         return shap.Explanation(
-            np.array(values, dtype=object),
-            np.array(base_values, dtype=object),
-            np.array(data, dtype=object),
-            feature_names=feature_names,
-            hierarchical_values=np.array(hierarchical_values),
-            clustering=np.array(clustering)
+            np.array([x.values[0, :, :] for x in shap_values], dtype=object),
+            np.array([x.base_values[0, :] for x in shap_values], dtype=object),
+            np.array([x.data[0, :] for x in shap_values], dtype=object),
+            feature_names=[x.feature_names[0] for x in shap_values],
+            clustering=np.array([x.clustering[0, :, :] for x in shap_values])
         )
 
     def _set_tokenizer(self, model: Any) -> None:
@@ -59,4 +47,28 @@ class SHAP(BaseExplainer):
 
         sentence_without_entities = self._create_sentence_without_entities(sample)
         explainer = shap.Explainer(_nested_predict, self.tokenizer_)
-        return explainer([sentence_without_entities], silent=True)
+        shap_value = explainer([sentence_without_entities], silent=True)
+        self._replace_values(shap_value, sample, index_1, index_2)
+        return shap.Explanation(
+            shap_value.values,
+            shap_value.base_values,
+            shap_value.data,
+            feature_names=shap_value.feature_names,
+            clustering=shap_value.clustering
+        )
+
+    @staticmethod
+    def _replace_values(
+            shap_value: shap.Explanation,
+            sample: dict,
+            index_1: int,
+            index_2: int
+    ) -> None:
+        entity_1 = "[E1] " + sample["tokens"][sample["index_1"]] + " [/E1] "
+        entity_2 = "[E2] " + sample["tokens"][sample["index_2"]] + " [/E2] "
+        index_1 = index_1 - 1 if index_1 > index_2 else index_1
+        shap_value.values = np.insert(shap_value.values, index_1+1, [0, 0], axis=1)
+        shap_value.values = np.insert(shap_value.values, index_2+1, [0, 0], axis=1)
+        shap_value.data = shap_value.data.astype(object)
+        shap_value.data = np.insert(shap_value.data, index_1+1, entity_1, axis=1)
+        shap_value.data = np.insert(shap_value.data, index_2+1, entity_2, axis=1)
