41fcad21ec08253a456817db1a7afb28ccca344a
Luiz Manke <luiz.manke@neoway.com.br>
2021-10-05 22:17:15 -0300

    Refactor dataset

--- relation_detection/_dataset.py
+++ relation_detection/_dataset.py
@@ -11,16 +11,14 @@ class Dataset:
         "news": News
     }
 
-    def __init__(self) -> None:
-        pass
-
-    def list_available_sets(self) -> List[str]:
-        return list(self.available_sets_.keys())
-
-    def load(self, dataset_name: str, file_path: Optional[str] = None) -> None:
-        assert dataset_name in self.available_sets_
+    def __init__(self, dataset_name: str, file_path: Optional[str] = None) -> None:
+        self._assert_dataset_name(dataset_name)
         self.dataset_ = self.available_sets_[dataset_name](file_path)
         self.dataset_.load()  # type: ignore
 
     def get_data(self) -> Tuple[List[dict], np.ndarray, List[str]]:
         return self.dataset_.get_data()  # type: ignore
+
+    def _assert_dataset_name(self, dataset_name:str) -> None:
+        if dataset_name not in self.available_sets_:
+            raise Exception(f"Dataset name not in {self.available_sets_.keys()}")
