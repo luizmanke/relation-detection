5b89fc9dc1a8cc8edcaf5792ff1c7dea0c06e9ed
Luiz Felipe Manke <luizmanke@gmail.com>
2022-03-05 22:31:33 -0300

    Refactor installation

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup, find_packages
 
 
-# get dependencies
-with open("requirements.txt") as req_file:
-    dependencies = req_file.read().splitlines()
-
-# ignore dev dependencies
-test_index = dependencies.index("# testing")
-dependencies = dependencies[:test_index]
+dependencies = [
+    "catboost==0.26",
+    "imbalanced-learn==0.8.0",
+    "ipython==7.26.0",
+    "lime==0.2.0.1",
+    "nltk==3.6.2",
+    "numpy==1.21.0",
+    "pandas==1.3.0",
+    "pyharmonysearch==1.4.3",
+    "scikit-learn==0.24.2",
+    "shap==0.39.0",
+    "spacy==3.1.0",
+    "termcolor==1.1.0",
+    "torch==1.9.0",
+    "transformers==4.8.2",
+    "types-termcolor==1.1.1"
+]
 
 setup(
     name="relation_detection",
