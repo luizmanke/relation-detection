a47817b83804f50b6c6044d74c641a459517d4bd
Luiz Felipe Manke <luizmanke@gmail.com>
2022-03-08 22:53:34 -0300

    Back to numpy array

--- relation_detection/_dataset.py
+++ relation_detection/_dataset.py
@@ -1,5 +1,5 @@
 from random import Random
-from typing import Dict, Optional
+from typing import Any, Dict, Optional
 from .datasets.dbpedia import DBpedia
 
 
@@ -18,18 +18,18 @@ class Dataset:
         self.dataset_ = self.available_sets_[dataset_name](file_path)
         self.n_samples_ = n_samples
 
-    def get_data(self) -> Dict[str, list]:
+    def get_data(self) -> Dict[str, Any]:
         if self.n_samples_:
             return self._random_sample()
         return self.dataset_.get_data()
 
-    def _random_sample(self) -> Dict[str, list]:
+    def _random_sample(self) -> Dict[str, Any]:
         data = self.dataset_.get_data()
         indexes = [i for i in range(len(data["samples"]))]
         Random(42).shuffle(indexes)
         indexes_selected = indexes[:self.n_samples_]
         return {
             "samples": [data["samples"][i] for i in indexes_selected],
-            "labels": [data["labels"][i] for i in indexes_selected],
+            "labels": data["labels"][indexes_selected],
             "groups": [data["groups"][i] for i in indexes_selected]
         }
