9718f8e8ed6450dc309ce229e17e067934637b56
Luiz Felipe Manke <luizmanke@gmail.com>
2022-03-08 22:19:36 -0300

    Remove spacy

--- relation_detection/utils.py
+++ relation_detection/utils.py
@@ -2,13 +2,6 @@ import spacy
 from termcolor import colored
 
 
-def download_spacy_model() -> None:
-    if not spacy.util.is_package("pt_core_news_lg"):
-        print("\nDownloading spacy model...")
-        spacy.cli.download("pt_core_news_lg", False, False, "--quiet")  # type: ignore
-        print("")
-
-
 def print_sentence(sample: dict) -> None:
     tokens = [token for token in sample["tokens"]]
     tokens[sample["index_1"]] = colored(tokens[sample["index_1"]], "blue", attrs=["bold"])
