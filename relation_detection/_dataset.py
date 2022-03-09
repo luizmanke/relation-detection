6771665273fb3e80de009c64c538bdb3d84594a7
Luiz Felipe Manke <luizmanke@gmail.com>
2022-03-08 21:55:10 -0300

    Limit number of samples

--- relation_detection/_dataset.py
+++ relation_detection/_dataset.py
@@ -1,5 +1,5 @@
-import numpy as np
-from typing import List, Tuple
+from random import Random
+from typing import Any, Dict, Optional
 from .datasets.dbpedia import DBpedia
 from .datasets.news import News
 
@@ -11,8 +11,27 @@ class Dataset:
         "news": News
     }
 
-    def __init__(self, dataset_name: str, file_path: str) -> None:
+    def __init__(
+        self,
+        dataset_name: str,
+        file_path: str,
+        n_samples: Optional[int] = None
+    ) -> None:
         self.dataset_ = self.available_sets_[dataset_name](file_path)
+        self.n_samples_ = n_samples
 
-    def get_data(self) -> Tuple[List[dict], np.ndarray, List[str]]:
+    def get_data(self) -> Dict[str, Any]:
+        if self.n_samples_:
+            return self._random_sample()
         return self.dataset_.get_data()
+
+    def _random_sample(self) -> Dict[str, Any]:
+        samples, labels, groups = self.dataset_.get_data()
+        indexes = [i for i in range(len(samples))]
+        Random(42).shuffle(indexes)
+        indexes_selected = indexes[:self.n_samples_]
+        return {
+            "samples": [samples[i] for i in indexes_selected],
+            "labels": labels[indexes_selected],
+            "groups": [groups[i] for i in indexes_selected]
+        }
